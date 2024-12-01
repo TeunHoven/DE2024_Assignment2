594143e6f2eb8553c78aba557b949600828b2c95
TeunHoven <teun.d.hoven@gmail.com>
2024-12-01 13:57:58 +0100

    Updated to final change in the database schema

--- stream-processing/kafka-consumer/multithread-consumer-window.py
+++ stream-processing/kafka-consumer/multithread-consumer-window.py
@@ -9,6 +9,7 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 
 from datetime import datetime
+from uuid import uuid4
 
 
 # if you want to learn about threading in python, check the following article
@@ -29,15 +30,63 @@ class KafkaMessageConsumer(Thread):
     def __init__(self, topic, time_between_reads=30, header="value"):
         Thread.__init__(self)
         self.topic = topic
-        self.consumer = KafkaConsumer(bootstrap_servers='34.30.212.168:9092',  # use your VM's external IP Here!
+        self.consumer = KafkaConsumer(bootstrap_servers='34.72.19.22:9092',  # use your VM's external IP Here!
                                       auto_offset_reset='earliest',
                                       consumer_timeout_ms=10000)
 
         self.consumer.subscribe(topics=[topic])
         self.time_between_reads = time_between_reads
         self.header = header
-        credentials = service_account.Credentials.from_service_account_file('stream-processing/kafka-consumer/service_worker.json')
-        self.client = bigquery.Client(credentials=credentials)
+        self.credentials = service_account.Credentials.from_service_account_file('stream-processing/kafka-consumer/service_worker.json')
+        self.client = bigquery.Client(credentials=self.credentials)
+
+    def get_window_id(self, window_start, window_length):
+        stmt = f"""
+            SELECT window_id
+            FROM Information.window
+            WHERE window_start = '{window_start}'
+            LIMIT 1;
+        """
+
+        query_job = self.client.query(stmt)
+        res = list(query_job.result())
+
+        # If the window is not in the database, add it
+        if len(res) == 0:
+            window_id = uuid4()
+
+            stmt = f"""
+                INSERT Information.window
+                (window_id, window_start, window_length)
+                VALUES ('{window_id}', '{window_start}', {window_length});
+            """
+
+
+            temp_client = bigquery.Client(credentials=self.credentials)
+
+            query_job = temp_client.query(stmt)
+            query_job.result()
+
+            return window_id
+        else:
+            return list(res)[0][0]
+
+    def get_service_id(self, service):
+        stmt = f"""
+            SELECT service_id
+            FROM Information.service
+            WHERE service_name = '{service}'
+            LIMIT 1;
+        """
+
+        query_job = self.client.query(stmt)
+        res = list(query_job.result())
+
+        # If the service is not in the database, add it
+        if len(res) == 0:
+            return None
+        else:
+            return list(res)[0][0]
 
     def update_table(self, data):
         all_values = []
@@ -54,14 +103,17 @@ class KafkaMessageConsumer(Thread):
 
             window_start = datetime.strptime(window_start, "%Y-%m-%d %H:%M:%S")
 
+            window_id = self.get_window_id(window_start, window_length)
+            service_id = self.get_service_id(service)
+
             value = int(value)
 
-            value = f"('{window_start}', {window_length}, '{service}', {value})"
+            value = f"('{window_id}', '{service_id}', {value})"
             all_values.append(value)
 
         stmt = f"""
-            INSERT Assignment_Data.{self.topic}_window
-            (window_start, window_length, service, amount_movies)
+            INSERT Assignment_Data.{self.topic}
+            (window_id, service_id, amount_movies)
             VALUES {",".join(all_values)};
         """
 
